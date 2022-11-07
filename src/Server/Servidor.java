bc2fdcb4edc3eb25fba67741a9f5e0d38cd63f65
Asuryu <tomas-250@outlook.com>
2022-11-07 01:37:13 +0000

    Alterada maneira como os servidores enviam e recebem o ficheiro DB

--- src/Server/Servidor.java
+++ src/Server/Servidor.java
@@ -5,6 +5,8 @@ import Server.Threads.*;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.TreeMap;
@@ -12,27 +14,27 @@ import java.util.stream.Collectors;
 
 public class Servidor {
 
-    public final String DATABASE_ORIGINAL = "PD-2022-23-TP.db";
-    public final String DATABASES_PATH;
-    public final String DATABASE_NAME;
-    public final String JDBC_STRING;
-    public Connection dbConn;
+    public final String DATABASE_ORIGINAL = "PD-2022-23-TP.db"; // Name of the original database file
+    public final String DATABASES_PATH; // Path to the folder where the databases are stored
+    public final String DATABASE_NAME; // Name of the database file
+    public final String JDBC_STRING; // JDBC string to connect to the database
+    public Connection dbConn; // Connection to the database
 
-    public ServerSocket s = null;
-    public int TCP_PORT;
+    public ServerSocket s; // Socket to receive TCP connections
+    public int TCP_PORT; // Port to receive TCP connections
 
-    public final int UDP_PORT;
-    public DatagramSocket ds = null;
+    public DatagramSocket ds; // Socket to receive UDP packets
+    public final int UDP_PORT; // Port to receive UDP packets
 
-    public final String MULTICAST_IP = "239.39.39.39";
-    public final int MULTICAST_PORT = 4004;
-    public InetAddress ipGroup;
-    public MulticastSocket ms;
-    public SocketAddress sa;
-    public NetworkInterface ni;
+    public final String MULTICAST_IP = "239.39.39.39"; // Multicast IP address (string)
+    public final int MULTICAST_PORT = 4004; // Multicast port
+    public InetAddress ipGroup; // Multicast IP address
+    public MulticastSocket ms; // Multicast socket
+    public SocketAddress sa; // Socket address
+    public NetworkInterface ni; // Network interface
 
-    public final ArrayList<Thread> threads = new ArrayList<>();
-    public final ArrayList<Heartbeat> onlineServers = new ArrayList<>();
+    public final ArrayList<Thread> threads = new ArrayList<>(); // List of threads
+    public final ArrayList<Heartbeat> onlineServers = new ArrayList<>(); // List of online servers
 
     public Servidor(int UDP_PORT, String DATABASES_PATH) throws Exception {
         this.UDP_PORT = UDP_PORT;
@@ -43,29 +45,39 @@ public class Servidor {
         s = new ServerSocket(0);
         TCP_PORT = s.getLocalPort();
 
+        // Começar à escuta por heartbeats (30 segundos)
         ThreadInicialHeartbeat tihb = new ThreadInicialHeartbeat(this);
         tihb.start();
         tihb.join(30000);
         ms.leaveGroup(sa, ni);
 
+        /* Se o servidor não tiver uma cópia local da base de dados
+        e existirem servidores com uma cópia, pede uma cópia ao que tiver
+        a base de dados com a versão mais atualizada e com menor carga */
         File copy = new File(DATABASES_PATH + DATABASE_NAME);
         if(!copy.exists() && !onlineServers.isEmpty()){
-            // get the server with the highest database version and the lowest load
+            // Get the server with the highest database version and the loweast load
             Heartbeat hb = onlineServers.stream()
                     .collect(Collectors.groupingBy(Heartbeat::getDbVersion, TreeMap::new, Collectors.toList()))
                     .lastEntry().getValue().stream()
                     .min(new HeartbeatComparatorLoad()).get();
 
+            onlineServers.removeIf(h -> h.getDbVersion() < 1); // TODO: (<1) Remove servers with no database
+
             Socket s = new Socket("localhost", hb.getPort());
             ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
             ObjectInputStream in = new ObjectInputStream(s.getInputStream());
-            out.writeObject(TCPMessages.GET_DATABASE);
+            FileOutputStream fos = new FileOutputStream(DATABASES_PATH + DATABASE_NAME);
+            out.writeObject(TCPMessages.GET_DATABASE); // Envia a mensagem ao servidor a pedir a base de dados
             out.flush();
-            File database = (File)in.readObject();
-            System.out.println("[ * ] Received database from server " + s.getInetAddress().getHostAddress() + ":" + hb.getPort());
-            FileOutputStream fos = new FileOutputStream(copy);
-            fos.write(database.getName().getBytes());
+            // Receber ficheiro aos poucos
+            byte[] msgByte = new byte[4000];
+            while(s.getInputStream().read(msgByte) != -1){
+                fos.write(msgByte);
+            }
+            System.out.println("[ · ] Received database from server " + s.getInetAddress().getHostAddress() + ":" + hb.getPort());
             fos.close();
+            s.close();
         }
 
 
--- src/Server/Threads/ThreadTCP.java
+++ src/Server/Threads/ThreadTCP.java
@@ -4,10 +4,14 @@ import Server.Servidor;
 import Server.TCPMessages;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * Classe que representa a thread que recebe as ligações através de TCP
+ */
 public class ThreadTCP extends Thread {
 
     private final Servidor server;
@@ -27,12 +31,22 @@ public class ThreadTCP extends Thread {
                   System.out.println("[ * ] Received TCP connection from " + client.getInetAddress().getHostAddress() + ":" + client.getPort());
                   ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
                   ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-                  TCPMessages request = (TCPMessages)in.readObject();
-                  switch (request){
-                      case GET_DATABASE:
-                          out.writeObject(new File(server.DATABASES_PATH + server.DATABASE_NAME));
+                  TCPMessages request = (TCPMessages)in.readObject(); // Obtém a mensagem do cliente
+                  switch (request) {
+                      case GET_DATABASE -> {
+                          System.out.println("[ * ] A server is requesting the database");
+                          File file = new File(server.DATABASES_PATH + server.DATABASE_NAME);
+                          FileInputStream fis = new FileInputStream(file);
+                          // Enviar ficheiro aos poucos
+                          int n;
+                          byte[] fileRead = new byte[4000];
+                          do {
+                              n = fis.read(fileRead);
+                              if (n == -1) break;
+                              client.getOutputStream().write(fileRead);
+                          } while (true);
                           System.out.println("[ * ] Sent database to server " + client.getInetAddress().getHostAddress() + ":" + client.getPort());
-                          break;
+                      }
                   }
                   client.close();
               }
@@ -40,10 +54,6 @@ public class ThreadTCP extends Thread {
               System.out.println("[ ! ] An error has occurred while receiving a TCP connection");
               System.out.println("      " + e.getMessage());
          }
-   }
-
-   private File getDatabaseFile(String path){
-         return new File(path);
    }
 
 }
