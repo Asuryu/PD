0569f195b8186fb7c7a0340713a4dc682d17e71f
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2022-11-16 19:38:24 +0000

    nada

--- src/Client/Cliente.java
+++ src/Client/Cliente.java
@@ -1,6 +1,10 @@
 package Client;
 
+
+
 import java.net.*;
+
+import java.util.Scanner;
 
 /**
  * Representa um cliente do sistema
@@ -21,5 +25,64 @@ public class Cliente {
         String live = "I m live";
         DatagramPacket datagramPacket = new DatagramPacket(live.getBytes(), live.length());
         datagramSocket.send(datagramPacket);
+
+
+        /*int flagCloseCmd=0;
+       do{
+           System.out.println("\n-->Command:");
+            Scanner sc  = new Scanner(System.in);
+            String cmd = sc.nextLine();
+            switch(cmd.toUpperCase())
+            {
+                case "EXIT":{
+                    flagCloseCmd=1;
+                    break;
+                }
+                case "REGISTER":{
+                    break;
+                }
+                case "LOGIN":{
+                    break;
+                }
+                case "EDIT DATA":{
+                    break;
+                }
+                case "CHECK RESERVATIONS TO PAY":{
+                    break;
+                }
+
+                case "CHECK PAID RESERVATIONS":{
+                    break;
+                }
+
+                case "SEARCH SPECTACLE":{
+                    break;
+                }
+
+                case "SELECT SPECTACLE":{
+                    break;
+                }
+
+                case "SEE AVAILABLE PLACES":{
+                    break;
+                }
+
+                case "SUBMIT RESERVATIONS":{
+                    break;
+                }
+
+                case "DELETE RESERVATIONS":{
+                    break;
+                }
+                case "PAY":{
+                    break;
+                }
+                case "LOGOUT":{
+                    break;
+                }
+            }
+        }while(flagCloseCmd==0);*/
+
+
     }
 }
