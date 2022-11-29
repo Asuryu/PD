bc26a8534b1457ec665467d751fc7c5a3da0fa47
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2022-11-29 19:14:31 +0000

    client sou burra

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,71 +1,71 @@
 package Client;
 
+import Server.Heartbeat;
 
-
-import   Client.UI.TextUserInterface;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
-
-      /*  String serverAddress;
-        int serverPort;
-        DatagramSocket mySocket = null;
-        DatagramPacket datagramPacket;
-        ByteArrayInputStream bin;
-        ObjectInputStream oin;
-
-        ByteArrayOutputStream bout;
-        ObjectOutputStream oout;*/
-
-      //  HashMap<String, Integer> serverList = new HashMap<String,Integer>();
-
-        if(args.length!=2){
-            System.out.println("Usage: java <name> <server address> <server port>");
+        System.out.println("A comecar o cliente....");
+        if (args.length != 2) {
+            System.err.println("[ERROR]Sintaxe: <lb address> <lb port>");
             return;
         }
-        System.out.println("A comecar o cliente....");
-        ClientObservavel clientObservavel = new ClientObservavel();
-        TextUserInterface userInterface = new TextUserInterface(clientObservavel);
-        userInterface.UI();
-        /*try
-        {
-            serverAddress = args[0];
-            serverPort = Integer.parseInt(args[1]);
-            mySocket = new DatagramSocket();
-            mySocket.setSoTimeout(TIMEOUT*1000);
-            byte[] buffer = new byte[MAX_PACKET_SIZE];
-
-            mySocket.connect(InetAddress.getByName(serverAddress), serverPort);
-
-            //Serializar a String TIME para um array de bytes encapsulado por bout
-            bout = new ByteArrayOutputStream();
-            oout = new ObjectOutputStream(bout);
-            oout.writeObject(TIMEOUT);
-            //Construir um datagrama UDP com o resultado da serialização
-            datagramPacket = new DatagramPacket(bout.toByteArray(), bout.size(), InetAddress.getByName(serverAddress), serverPort);
-            mySocket.send(datagramPacket);
-            datagramPacket = new DatagramPacket(new byte[MAX_PACKET_SIZE], MAX_PACKET_SIZE);
-
-            mySocket.receive(datagramPacket);
-            bin = new ByteArrayInputStream(datagramPacket.getData(), 0, datagramPacket.getLength());
-            oin = new ObjectInputStream(bin);
-
-
-        } catch (UnknownHostException e) {
-        System.out.println("Destino desconhecido:\n\t" + e);
-        } catch (NumberFormatException e) {
-        System.out.println("O porto do servidor deve ser um inteiro positivo.");
-        } catch (SocketTimeoutException e) {
-        System.out.println("Nao foi recebida qualquer resposta:\n\t" + e);
-        } catch (SocketException e) {
-        System.out.println("Ocorreu um erro ao nivel do socket UDP:\n\t" + e);
-        } catch (IOException e) {
-        System.out.println("Ocorreu um erro no acesso ao socket:\n\t" + e);
+     /*   try{
+            new Client(args[0],Integer.parseInt(args[1]));
+        }catch (Exception e) {
+            System.out.println("[ ! ] An error has occurred while setting up the client");
+            System.out.println("      " + e.getMessage());
+            e.printStackTrace();
         }*/
 
+        int port;
+        String ip;
 
+        ip = args[0];
+        port = Integer.parseInt(args[1]);
+
+        DatagramSocket datagramSocket = new DatagramSocket();
+        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(bOut);
+
+        datagramSocket.connect( InetAddress.getByName(ip),port);
+
+        String live = "REGISTER";
+        DatagramPacket datagramPacket = new DatagramPacket(live.getBytes(), live.length());
+        datagramSocket.send(datagramPacket);
+
+        // Recebe a lista de servidores disponíveis
+        DatagramPacket dp = new DatagramPacket(new byte[4096], 4096);
+        datagramSocket.receive(dp);
+        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(dp.getData(), 0, dp.getLength()));
+        ArrayList<Heartbeat> servers = (ArrayList<Heartbeat>) in.readObject();
+        System.out.println(Arrays.toString(servers.toArray()));
+
+        for(Heartbeat heartbeat : servers){
+            try{
+                // Socket socket = new Socket("localhost",heartbeat.getPort());
+                Socket socket = new Socket("10.65.132.193",heartbeat.getPort());
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                String log [] = {"REGISTER","tania","tania","123"};
+
+                objectOutputStream.writeObject("REGISTER tania tania 123");
+                objectOutputStream.flush();
+
+                String read = (String)objectInputStream.readObject();
+                System.out.println("read = "+ read);
+            }catch (Exception e){
+                System.out.println("[ ! ] An error has occurred while receiving a TCP connection");
+                // System.out.println("      " + e.printStackTrace();
+                e.printStackTrace();
+            }
+        }
     }
-
-
 }
--- src/Client/shared/Answers.java
+++
@@ -1,5 +0,0 @@
-package Client.shared;
-
-//Classes que vão ser enviadas em resposta de algo
-
-public interface Answers {}
