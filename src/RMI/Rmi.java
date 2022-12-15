352359d5097b7cbb1fd3732a080752a8ab6f7c45
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2022-12-15 18:23:44 +0000

    menu rmi

--- src/RMI/Rmi.java
+++ src/RMI/Rmi.java
@@ -1,6 +1,9 @@
 package RMI;
 
 
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
@@ -22,9 +25,31 @@ public class Rmi {
             LOGGER.log(Level.SEVERE, "Invalid number of arguments! Usage: <rmi service address>");
             return;
         }
-
-      //  Scanner sysReader = new Scanner(System.in);
         System.out.println("I m here");
+        Scanner scanner = new Scanner(System.in);
+
+        LOGGER.log(Level.INFO, "RMI service created and running.");
+
+        int option;
+
+            System.out.println("SERVICE RUNNING! ASYNCHRONOUS NOTIFICATIONS WILL BE PRINTED AUTOMATICALLY");
+            System.out.println("OPTIONS:");
+            System.out.println("\t1 -> Listar os servidores ativos");
+            System.out.println("\t2 -> Terminar");
+            System.out.println("\nResposta:");
+
+            option = scanner.nextInt();
+
+          switch (option){
+              case 1:
+                  System.out.println("Vamos la listar os servidores");
+                  break;
+              case 2:
+                  System.out.println("Saindo");
+                  break;
+          }
+
+
     }
 
     private static void prepareLogger() {
