a2e5d8b81bdc4ac9b3307b9b420bc4ff246c4da1
Tomás Silva <tomas-250@outlook.com>
2022-12-19 00:59:45 +0000

    REST API start!

---
+++ REST API/src/test/java/pd/grupo5/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pd.grupo5.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
