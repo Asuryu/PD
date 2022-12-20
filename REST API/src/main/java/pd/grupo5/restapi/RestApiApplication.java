450a0f1ff6557e878f4d88e52482df9592d9f3bf
Asuryu <tomas-250@outlook.com>
2022-12-20 21:11:23 +0000

    Authorization

--- REST API/src/main/java/pd/grupo5/restapi/RestApiApplication.java
+++ REST API/src/main/java/pd/grupo5/restapi/RestApiApplication.java
@@ -2,7 +2,10 @@ package pd.grupo5.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pd.grupo5.restapi.config.RsaKeyProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class RestApiApplication {
 
